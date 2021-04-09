39f0cd3752295329ce430b39562250415681d768
Tristan Nelson <thnelson@geisinger.edu>
2021-04-09 10:45:54 -0400

    adding gvdev config

--- terraform/dev/genegraph-gvdev.tf
+++ terraform/dev/genegraph-gvdev.tf
@@ -1,13 +1,13 @@
 resource "google_compute_global_address" "genegraph_gvdev_external_ip" {
-  name = "global-dev-genegraph-dev-ip"
+  name = "global-dev-genegraph-gvdev-ip"
 }
 
-# NOT managed, just referenecd
-data "google_dns_managed_zone" "clingen_dns_zone" {
-  name = "clingen-app"
-}
+# # NOT managed, just referenecd
+# data "google_dns_managed_zone" "clingen_dns_zone" {
+#   name = "clingen-app"
+# }
 
-resource "google_dns_record_set" "argo_a_record" {
+resource "google_dns_record_set" "genegraph_gvdev_a_record" {
   name = "genegraph-gvdev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
   type = "A"
   ttl  = 300
