3bf66b7bb911acef44e114743277f0137c788ac0
sjahl <sjahl@broadinstitute.org>
2021-04-09 13:22:03 -0400

    consolidate dns records in dev

--- terraform/dev/dns.tf
+++ terraform/dev/dns.tf
@@ -1,12 +1,19 @@
-resource "google_compute_global_address" "argocd_external_ip" {
-  name = "global-dev-argocd-ip"
-}
-
+# Clingen.app dns zone
 # NOT managed, just referenecd
 data "google_dns_managed_zone" "clingen_dns_zone" {
   name = "clingen-app"
 }
 
+# static IP reservations
+resource "google_compute_global_address" "argocd_external_ip" {
+  name = "global-dev-argocd-ip"
+}
+
+resource "google_compute_global_address" "genegraph_gvdev_external_ip" {
+  name = "global-dev-genegraph-gvdev-ip"
+}
+
+# DNS records
 resource "google_dns_record_set" "argo_a_record" {
   name = "argocd-dev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
   type = "A"
@@ -15,6 +22,16 @@ resource "google_dns_record_set" "argo_a_record" {
   managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
 
   rrdatas = [google_compute_global_address.argocd_external_ip.address]
+}
+
+resource "google_dns_record_set" "genegraph_gvdev_a_record" {
+  name = "genegraph-gvdev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_gvdev_external_ip.address]
 }
 
 # delegated sub zones
