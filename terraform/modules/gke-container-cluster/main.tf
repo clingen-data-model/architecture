9ab64c4afc26e3abbd6fde12f8ab082e0134fb94
sjahl <sjahl@broadinstitute.org>
2021-04-15 10:24:13 -0400

    add in conditional for maint policy

--- terraform/modules/gke-container-cluster/main.tf
+++ terraform/modules/gke-container-cluster/main.tf
@@ -7,5 +7,18 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  if var.enable_maintenance_policy {
+    maintenance_policy {
+      recurring_window {
+        # start_time = "2021-03-24T11:00:00Z"
+        start_time = var.maint_start_timespec
+        # end_time   = "2021-03-24T23:00:00Z"
+        end_time   = var.maint_end_timespec
+        # recurrence = "FREQ=WEEKLY;BYDAY=SA,SU"
+        recurrence = var.maint_recurrence_spec
+      }
+    }
+  }
 }
 
