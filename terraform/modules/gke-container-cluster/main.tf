4d4b421d81bf5d9c206b90a8070202a7d5426567
sjahl <sjahl@broadinstitute.org>
2021-04-15 15:04:00 -0400

    format and set default values

--- terraform/modules/gke-container-cluster/main.tf
+++ terraform/modules/gke-container-cluster/main.tf
@@ -5,7 +5,7 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
+  remove_default_node_pool = var.remove_default_node_pool
   initial_node_count       = 1
 
   maintenance_policy {
--- terraform/modules/gke-container-cluster/variables.tf
+++ terraform/modules/gke-container-cluster/variables.tf
@@ -1,16 +1,19 @@
 variable "remove_default_node_pool" {
-    type = boolean
-    default = false
+  type    = boolean
+  default = false
 }
 
 variable "maint_recurring_windows" {
-  type = list(map(object({start_time = string, end_time = string, recurrence = string})))
+  type    = list(map(object({ start_time = string, end_time = string, recurrence = string })))
+  default = []
 }
 
 variable "maint_daily_windows" {
-    type = list(map(object({start_time = string})))
+  type    = list(map(object({ start_time = string })))
+  default = []
 }
 
 variable "maint_exclusions" {
-    type = list(map(object({name = string, start_time = string, end_time = string})))
+  type    = list(map(object({ name = string, start_time = string, end_time = string })))
+  default = []
 }
