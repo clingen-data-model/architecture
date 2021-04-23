d8eb9464a5fb4c2325bbdd95b8b606efdf1816d6
sjahl <sjahl@broadinstitute.org>
2021-04-23 15:20:36 -0400

    fixups from testing the submitter deployment

--- terraform/stage/cloudbuild_triggers.tf
+++ terraform/stage/cloudbuild_triggers.tf
@@ -1,4 +1,7 @@
 resource "google_cloudbuild_trigger" "clinvar_submitter_push" {
+  name = "clinvar-submitter-stage-build"
+  description = "Build clinvar submitter on push to master"
+
   github {
     name   = "clinvar-submitter"
     owner       = "clingen-data-model"
