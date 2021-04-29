d8eeff08697a28f88290b8a38f75503408d870d1
sjahl <sjahl@broadinstitute.org>
2021-04-29 10:14:28 -0400

    change datespec in maint window to avoid a tf change

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -9,13 +9,13 @@ module "external-secrets" {
 }
 
 module "prod-gke-cluster" {
-  source                    = "git@github.com:broadinstitute/tgg-terraform-modules.git//imported-gke-cluster?ref=ce78edb118f2cd4bdb9d0adec33aa323b92f7a2f"
+  source                    = "git@github.com:broadinstitute/tgg-terraform-modules.git//imported-gke-cluster?ref=26fcbffd5441e69e2b3e5fb6fe4e66d0e66a0bbc"
   cluster_name              = "prod-cluster"
   cluster_location          = "us-east1-b"
   network_id                = "projects/clingen-dx/global/networks/default"
   subnetwork_id             = "projects/clingen-dx/regions/us-east1/subnetworks/default"
-  maint_start_time          = "2021-03-24T11:00:00Z"
-  maint_end_time            = "2021-03-24T23:00:00Z"
+  maint_start_time          = "2021-03-02T11:00:00Z"
+  maint_end_time            = "2021-03-02T23:00:00Z"
   maint_recurrence_sched    = "FREQ=WEEKLY;BYDAY=SA,SU"
   initial_node_count        = 0
   default_pool_node_count   = 3
