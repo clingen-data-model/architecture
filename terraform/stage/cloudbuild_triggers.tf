48b69e378f5b08f3f88c68c30b1ec692bb09e401
sjahl <sjahl@broadinstitute.org>
2021-05-17 13:17:13 -0400

    attempt at a cloudbuild trigger that only runs on PRs where helm has changed

--- terraform/stage/cloudbuild_triggers.tf
+++ terraform/stage/cloudbuild_triggers.tf
@@ -1,3 +1,4 @@
+# clinvar-submitter build
 resource "google_cloudbuild_trigger" "clinvar_submitter_push" {
   name = "clinvar-submitter-stage-build"
   description = "Build clinvar submitter on push to master"
@@ -11,4 +12,24 @@ resource "google_cloudbuild_trigger" "clinvar_submitter_push" {
   }
 
   filename = "cloudbuild.yaml"
+}
+
+# architecture helm chart linting
+resource "google_cloudbuild_trigger" "architecture_helm_lint" {
+  name = "architecture-helm-pr-lint"
+  description = "lint helm charts in architecture when they've changed"
+
+  github {
+    name = "architecture"
+    owner = "clingen-data-model"
+    pull_request {
+      branch = "^master$"
+    }
+  }
+
+  included_files = [
+    "helm/**"
+  ]
+
+  filename = "helm/cloudbuild.yaml"
 }
