da84553b3fba773e1aa9f05aca0921438a0b0f33
sjahl <sjahl@broadinstitute.org>
2021-05-17 14:33:55 -0400

    terraform fmt

--- terraform/stage/cloudbuild_triggers.tf
+++ terraform/stage/cloudbuild_triggers.tf
@@ -1,13 +1,13 @@
 # clinvar-submitter build
 resource "google_cloudbuild_trigger" "clinvar_submitter_push" {
-  name = "clinvar-submitter-stage-build"
+  name        = "clinvar-submitter-stage-build"
   description = "Build clinvar submitter on push to master"
 
   github {
-    name   = "clinvar-submitter"
-    owner       = "clingen-data-model"
+    name  = "clinvar-submitter"
+    owner = "clingen-data-model"
     push {
-        branch = "^master$"
+      branch = "^master$"
     }
   }
 
@@ -16,11 +16,11 @@ resource "google_cloudbuild_trigger" "clinvar_submitter_push" {
 
 # architecture helm chart linting
 resource "google_cloudbuild_trigger" "architecture_helm_lint" {
-  name = "architecture-helm-pr-lint"
+  name        = "architecture-helm-pr-lint"
   description = "lint helm charts in architecture when they've changed"
 
   github {
-    name = "architecture"
+    name  = "architecture"
     owner = "clingen-data-model"
     pull_request {
       branch = "^master$"
