f437252796d0f343eba385f231d76bc863a66a05
Tristan Nelson <thnelson@geisinger.edu>
2021-05-20 10:44:45 -0400

    Add genegraph-dev service

--- terraform/dev/dns.tf
+++ terraform/dev/dns.tf
@@ -9,6 +9,10 @@ resource "google_compute_global_address" "genegraph_gvdev_external_ip" {
   name = "global-dev-genegraph-gvdev-ip"
 }
 
+resource "google_compute_global_address" "genegraph_dev_external_ip" {
+  name = "global-dev-genegraph-dev-ip"
+}
+
 # DNS records
 resource "google_dns_record_set" "genegraph_gvdev_a_record" {
   name = "genegraph-gvdev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
@@ -20,3 +24,24 @@ resource "google_dns_record_set" "genegraph_gvdev_a_record" {
   rrdatas = [google_compute_global_address.genegraph_gvdev_external_ip.address]
 }
 
+# DNS records
+resource "google_dns_record_set" "genegraph_dev_a_record" {
+  name = "genegraph-dev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_dev_external_ip.address]
+}
+
+# DNS records
+resource "google_dns_record_set" "genegraph_testing_a_record" {
+  name = "genegraph-testing.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_dev_external_ip.address]
+}
