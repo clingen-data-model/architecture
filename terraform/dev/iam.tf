568255a63a237aab5524cdd4468b2ab46aab7fec
sjahl <sjahl@broadinstitute.org>
2021-07-20 11:01:56 -0400

    add some of our other group perms

--- terraform/dev/iam.tf
+++ terraform/dev/iam.tf
@@ -1,12 +1,15 @@
+# project-level permissions that need to be consistent across all three environments
 module "clingen_projects_iam_bindings" {
   source  = "terraform-google-modules/iam/google//modules/projects_iam"
   version = "7.2.0"
+  mode = "additive"
 
   projects = ["clingen-dx", "clingen-stage", "clingen-dev"]
 
   bindings = {
     "roles/storage.admin" = [
       "group:clingendevs@broadinstitute.org",
+      "group:<collaborators-tbd>@broadinstitute.org",
     ]
 
     "roles/container.admin" = [
@@ -15,6 +18,7 @@ module "clingen_projects_iam_bindings" {
 
     "roles/compute.admin" = [
       "group:clingendevs@broadinstitute.org",
+      "group:<collaborators-tbd>@broadinstitute.org",
     ]
 
     "roles/appengine.appCreator" = [
@@ -26,43 +30,60 @@ module "clingen_projects_iam_bindings" {
     ]
 
     "roles/cloudfunctions.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/run.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/cloudscheduler.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/dns.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
+      "group:<collaborators-tbd>@broadinstitute.org",
+
     ]
 
     "roles/bigquery.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/monitoring.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/pubsub.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/firebase.admin" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/cloudbuild.builds.editor" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
     ]
 
     "roles/billing.viewer" = [
-      "group:clingendevs@broadinstitute.org"
+      "group:clingendevs@broadinstitute.org",
+    ]
+  }
+}
+
+# grants for specific storage buckets
+module "clingen_storage_buckets_iam" {
+  source  = "terraform-google-modules/iam/google//modules/storage_buckets_iam"
+  version = "7.2.0"
+  mode    = "additive"
+
+  storage_buckets = ["clinvar-reports"]
+
+    bindings = {
+    "roles/storage.objectViewer" = [
+      "<clingen-data-viewers-tbd>@broadinstitute.org",
     ]
   }
 }
