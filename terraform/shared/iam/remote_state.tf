8c1c8f3b72cad238ceaabc09ee183dab16084943
sjahl <sjahl@broadinstitute.org>
2021-08-05 10:30:02 -0400

    namespace the remote state

--- terraform/shared/iam/remote_state.tf
+++ terraform/shared/iam/remote_state.tf
@@ -1,13 +1,12 @@
 resource "google_storage_bucket_iam_member" "member" {
-  bucket  = "clingen-tfstate-shared"
-  role    = "roles/storage.objectAdmin"
-  member  = "group:clingendevs@broadinstitute.org"
-  project = "clingen-dx"
+  bucket = "clingen-tfstate-shared"
+  role   = "roles/storage.objectAdmin"
+  member = "group:clingendevs@broadinstitute.org"
 }
 
 terraform {
   backend "gcs" {
     bucket = "clingen-tfstate-shared"
-    prefix = "terraform/state"
+    prefix = "terraform/iam/state"
   }
 }
