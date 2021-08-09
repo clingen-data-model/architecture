8b73acfa1d913c790b1e6b5127eb9d7183202691
sjahl <sjahl@broadinstitute.org>
2021-08-09 13:14:37 -0400

    remove default cross series reducer to squelch some tf plan jitter

--- terraform/prod/monitoring.tf
+++ terraform/prod/monitoring.tf
@@ -119,7 +119,6 @@ resource "google_monitoring_alert_policy" "prod_node_allocatable_memory_utilizat
       aggregations {
         alignment_period     = "360s"
         per_series_aligner   = "ALIGN_MEAN"
-        cross_series_reducer = "REDUCE_NONE"
       }
     }
   }
