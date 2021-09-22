4cf16492a56c3670c8515aadbacd5ae2ec4bff00
Terry ONeill <toneill@broadinstitute.org>
2021-09-22 16:21:40 -0400

    Update helm and terraform configurations for stage and prod migration parity.

--- terraform/prod/dns.tf
+++ terraform/prod/dns.tf
@@ -53,3 +53,18 @@ resource "google_dns_record_set" "clinvar_submitter_a_record" {
   rrdatas = [google_compute_global_address.clinvar_submitter_ip.address]
 }
 
+resource "google_compute_global_address" "genegraph_prod_external_ip" {
+  name = "global-prod-genegraph-prod-ip"
+}
+
+# DNS Records in the prod.clingen.app zone
+resource "google_dns_record_set" "genegraph_prod_a_record" {
+  name = "genegraph.${google_dns_managed_zone.clingen_prod_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.clingen_prod_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_prod_external_ip.address]
+}
+
