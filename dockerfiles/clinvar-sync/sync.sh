13b5599cee379935b81491481ee81a93cb732135
sjahl <sjahl@broadinstitute.org>
2021-11-19 09:55:00 -0500

    initial dockerfile

---
+++ dockerfiles/clinvar-sync/sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GTOKEN=`gcloud auth application-default print-access-token --quiet`
+
+# get the most recent dataset name that exists in the clingen-stage project
+max_ds=`eval "bq query --use_legacy_sql=false --quiet --format=csv \"SELECT MAX(schema_name) FROM clingen-stage.INFORMATION_SCHEMA.SCHEMATA WHERE REGEXP_CONTAINS(schema_name, r'clinvar_[0-9]{4}_[0-9]{2}_[0-9]{2}')\" | awk '{if(NR>1)print}'"`
+
+# get any datasets and their ids that are greater than the most recent existing one
+x=`curl -s -X GET "https://data.terra.bio/api/repository/v1/snapshots?direction=asc&filter=clinvar&limit=10000&offset=0&sort=name" -H "accept: application/json" -H "authorization: Bearer $GTOKEN"| jq -r '.items[] | select(.name > "'${max_ds}'") | "\(.id) \(.name)"'`
+
+if [ -z "$x" ]
+then
+      echo "No new datasets to process."
+      exit 0
+fi
+
+# loop through them, get the data project info and create the new datastset in clinge-stage
+echo "$x" | while read id name ; do
+    dataProject=`curl -s -X GET "https://data.terra.bio/api/repository/v1/snapshots/$id?include=DATA_PROJECT" -H "accept: application/json" -H "authorization: Bearer $GTOKEN" | jq .dataProject | tr -d '"'`
+    echo "bq query --use_legacy_sql=false  'CALL \`clingen-stage.clinvar_qa.create_clinvar_dataset\`("\""${dataProject}.${name}"\"", "\""clingen-stage.${name}"\"")'"
+    # eval "bq query --use_legacy_sql=false  'CALL \`clingen-stage.clinvar_qa.create_clinvar_dataset\`("\""${dataProject}.${name}"\"", "\""clingen-stage.${name}"\"")'"
+    echo "Created view clingen-stage.${name} based on ${dataProject}.${name}"
+done
+
+echo "complete!"
