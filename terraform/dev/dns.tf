aa10d445a8991ca5532729271a0fe29233dfde50
Kyle Ferriter <kferrite@broadinstitute.org>
2022-07-12 14:49:10 -0500

    Add anyvar-dev static IP and A record

--- terraform/dev/dns.tf
+++ terraform/dev/dns.tf
@@ -17,6 +17,10 @@ resource "google_compute_global_address" "genegraph_dev_clinvar_external_ip" {
   name = "global-dev-genegraph-clinvar-ip"
 }
 
+resource "google_compute_global_address" "anyvar_dev_external_ip" {
+  name = "global-anyvar-dev-ip"
+}
+
 # DNS records
 resource "google_dns_record_set" "genegraph_gvdev_a_record" {
   name = "genegraph-gvdev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
@@ -28,7 +32,6 @@ resource "google_dns_record_set" "genegraph_gvdev_a_record" {
   rrdatas = [google_compute_global_address.genegraph_gvdev_external_ip.address]
 }
 
-# DNS records
 resource "google_dns_record_set" "genegraph_dev_a_record" {
   name = "genegraph-dev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
   type = "A"
@@ -39,7 +42,6 @@ resource "google_dns_record_set" "genegraph_dev_a_record" {
   rrdatas = [google_compute_global_address.genegraph_dev_external_ip.address]
 }
 
-# DNS records
 resource "google_dns_record_set" "genegraph_dev_clinvar_a_record" {
   name = "genegraph-dev-clinvar.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
   type = "A"
@@ -50,7 +52,6 @@ resource "google_dns_record_set" "genegraph_dev_clinvar_a_record" {
   rrdatas = [google_compute_global_address.genegraph_dev_clinvar_external_ip.address]
 }
 
-# DNS records
 resource "google_dns_record_set" "genegraph_testing_a_record" {
   name = "genegraph-testing.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
   type = "A"
@@ -59,4 +60,14 @@ resource "google_dns_record_set" "genegraph_testing_a_record" {
   managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
 
   rrdatas = [google_compute_global_address.genegraph_dev_external_ip.address]
+}
+
+resource "google_dns_record_set" "anyvar_dev_a_record" {
+  name = "anyvar-dev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_dev_clinvar_external_ip.address]
 }
