f03d84a67a71850d0ae8c517d4166b196f4be0ea
Kyle Ferriter <kferrite@broadinstitute.org>
2023-02-07 14:43:11 -0500

    Fix uta_setup vars

--- helm/charts/clingen-vicc/docker/uta/uta_setup.sh
+++ helm/charts/clingen-vicc/docker/uta/uta_setup.sh
@@ -3,8 +3,9 @@
 set -xeo pipefail
 
 if [ -z "$UTA_VERSION" ]; then
-    export UTA_VERSION=uta_20210129.pgd.gz
+    export UTA_VERSION=uta_20210129
 fi
+
 if [ -z "$UTA_ADMIN_PASSWORD" ]; then
     echo "Must provide nonempty env var UTA_ADMIN_PASSWORD"
     exit 1
@@ -15,9 +16,9 @@ if [ ! -f "$initialized_file" ]; then
     createuser -U postgres uta_admin
     createuser -U postgres anonymous
     createdb -U postgres -O uta_admin uta
-    psql -U postgres -c "ALTER USER uta_admin WITH PASSWORD $UTA_ADMIN_PASSWORD"
+    psql -U postgres -c "ALTER USER uta_admin WITH PASSWORD '$UTA_ADMIN_PASSWORD'"
 
-    curl -s http://dl.biocommons.org/uta/$UTA_VERSION \
+    curl -s "http://dl.biocommons.org/uta/${UTA_VERSION}.pgd.gz" \
         | gzip -cdq  \
         | grep -v "^REFRESH MATERIALIZED VIEW" \
         | psql -U uta_admin -d uta --echo-errors --single-transaction -v ON_ERROR_STOP=1
