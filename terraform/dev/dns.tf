02d5a9f8c445a6556ad9bb67c0dee6206072eac4
Tristan Nelson <thnelson@geisinger.edu>
2024-01-24 10:14:16 -0500

    Adding IP for genegraph-gene-validity-dev

--- terraform/dev/dns.tf
+++ terraform/dev/dns.tf
@@ -21,6 +21,10 @@ resource "google_compute_global_address" "anyvar_dev_external_ip" {
   name = "global-anyvar-dev-ip"
 }
 
+resource "google_compute_global_address" "genegraph_gene_validity_dev_external_ip" {
+  name = "global-dev-genegraph-gene-validity-ip"
+}
+
 
 # DNS records
 resource "google_dns_record_set" "genegraph_gvdev_a_record" {
@@ -31,6 +35,16 @@ resource "google_dns_record_set" "genegraph_gvdev_a_record" {
   managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
 
   rrdatas = [google_compute_global_address.genegraph_gvdev_external_ip.address]
+}
+
+resource "google_dns_record_set" "genegraph_gene_validity_dev_a_record" {
+  name = "genegraph-gene-validity-dev.${data.google_dns_managed_zone.clingen_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.clingen_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_gene_validity_dev_external_ip.address]
 }
 
 resource "google_dns_record_set" "genegraph_dev_a_record" {
