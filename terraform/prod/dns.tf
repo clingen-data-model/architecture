04b45c3673d3e16088cacc4df81e83d7244760eb
Tristan Nelson <thnelson@geisinger.edu>
2025-06-25 17:08:44 -0400

    Adding IP for genegraph-legacy-test

--- terraform/prod/dns.tf
+++ terraform/prod/dns.tf
@@ -95,3 +95,17 @@ resource "google_dns_record_set" "genegraph_api_prod_a_record" {
 
   rrdatas = [google_compute_global_address.genegraph_api_prod_external_ip.address]
 }
+
+resource "google_compute_global_address" "genegraph_legacy_test_external_ip" {
+  name = "global-genegraph-legacy-test-ip"
+}
+
+resource "google_dns_record_set" "genegraph_legacy_test_prod_a_record" {
+  name = "genegraph-legacy-test.${google_dns_managed_zone.clingen_prod_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.clingen_prod_dns_zone.name
+
+  rrdatas = [google_compute_global_address.genegraph_legacy_test_external_ip.address]
+}
