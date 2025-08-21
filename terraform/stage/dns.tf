a57168a8c1b78a7668ea14c29dfe49c2809d34a0
Kyle Ferriter <kferrite@broadinstitute.org>
2025-08-21 16:12:43 -0400

    Remove GKE stage cluster from terraform and argo. Remove stage and dev argo apps (#659)

--- terraform/stage/dns.tf
+++ terraform/stage/dns.tf
@@ -22,91 +22,91 @@ resource "google_dns_record_set" "clingen_stage_dns_zone_ns" {
 }
 
 # Reserved static IP addresses
-resource "google_compute_global_address" "clinvar_submitter_ip" {
-  name = "global-stage-clinvar-submitter-ip"
-}
+# resource "google_compute_global_address" "clinvar_submitter_ip" {
+#   name = "global-stage-clinvar-submitter-ip"
+# }
 
-resource "google_compute_global_address" "genegraph_stage_external_ip" {
-  name = "clingen-ds-stage-ip"
-}
+# resource "google_compute_global_address" "genegraph_stage_external_ip" {
+#   name = "clingen-ds-stage-ip"
+# }
 
-resource "google_compute_global_address" "genegraph_stage_clinvar_external_ip" {
-  name = "clingen-genegraph-stage-clinvar-ip"
-}
+# resource "google_compute_global_address" "genegraph_stage_clinvar_external_ip" {
+#   name = "clingen-genegraph-stage-clinvar-ip"
+# }
 
-resource "google_compute_global_address" "genegraph_gene_validity_stage_external_ip" {
-  name = "global-stage-genegraph-gene-validity-ip"
-}
+# resource "google_compute_global_address" "genegraph_gene_validity_stage_external_ip" {
+#   name = "global-stage-genegraph-gene-validity-ip"
+# }
 
-resource "google_compute_global_address" "genegraph_api_stage_external_ip" {
-  name = "global-stage-genegraph-api-ip"
-}
+# resource "google_compute_global_address" "genegraph_api_stage_external_ip" {
+#   name = "global-stage-genegraph-api-ip"
+# }
 
 # DNS Records in the stage.clingen.app zone
-resource "google_dns_record_set" "clinvar_submitter_a_record" {
-  name = "clinvar-submitter.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
-  type = "A"
-  ttl  = 300
+# resource "google_dns_record_set" "clinvar_submitter_a_record" {
+#   name = "clinvar-submitter.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
+#   managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
 
-  rrdatas = [google_compute_global_address.clinvar_submitter_ip.address]
-}
+#   rrdatas = [google_compute_global_address.clinvar_submitter_ip.address]
+# }
 
-resource "google_dns_record_set" "genegraph_stage_a_record" {
-  name = "genegraph.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
-  type = "A"
-  ttl  = 300
+# resource "google_dns_record_set" "genegraph_stage_a_record" {
+#   name = "genegraph.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
+#   managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
 
-  rrdatas = [google_compute_global_address.genegraph_stage_external_ip.address]
-}
+#   rrdatas = [google_compute_global_address.genegraph_stage_external_ip.address]
+# }
 
-resource "google_dns_record_set" "genegraph_stage_clinvar_a_record" {
-  name = "genegraph-clinvar.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
-  type = "A"
-  ttl  = 300
+# resource "google_dns_record_set" "genegraph_stage_clinvar_a_record" {
+#   name = "genegraph-clinvar.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
+#   managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
 
-  rrdatas = [google_compute_global_address.genegraph_stage_clinvar_external_ip.address]
-}
+#   rrdatas = [google_compute_global_address.genegraph_stage_clinvar_external_ip.address]
+# }
 
-resource "google_dns_record_set" "genegraph_gene_validity_stage_a_record" {
-  name = "genegraph-gene-validity.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
-  type = "A"
-  ttl  = 300
+# resource "google_dns_record_set" "genegraph_gene_validity_stage_a_record" {
+#   name = "genegraph-gene-validity.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
+#   managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
 
-  rrdatas = [google_compute_global_address.genegraph_gene_validity_stage_external_ip.address]
-}
+#   rrdatas = [google_compute_global_address.genegraph_gene_validity_stage_external_ip.address]
+# }
 
 
-resource "google_dns_record_set" "genegraph_api_stage_a_record" {
-  name = "genegraph-api.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
-  type = "A"
-  ttl  = 300
+# resource "google_dns_record_set" "genegraph_api_stage_a_record" {
+#   name = "genegraph-api.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
+#   managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
 
-  rrdatas = [google_compute_global_address.genegraph_api_stage_external_ip.address]
-}
+#   rrdatas = [google_compute_global_address.genegraph_api_stage_external_ip.address]
+# }
 
 
 # Genegraph Gene Validity Dev instance
 
-resource "google_compute_global_address" "genegraph_gene_validity_stage_dev_external_ip" {
-  name = "global-stage-dev-genegraph-gene-validity-ip"
-}
+# resource "google_compute_global_address" "genegraph_gene_validity_stage_dev_external_ip" {
+#   name = "global-stage-dev-genegraph-gene-validity-ip"
+# }
 
-resource "google_dns_record_set" "genegraph_gene_validity_stage_dev_a_record" {
-  name = "genegraph-gene-validity-dev.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
-  type = "A"
-  ttl  = 300
+# resource "google_dns_record_set" "genegraph_gene_validity_stage_dev_a_record" {
+#   name = "genegraph-gene-validity-dev.${google_dns_managed_zone.clingen_stage_dns_zone.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
+#   managed_zone = google_dns_managed_zone.clingen_stage_dns_zone.name
 
-  rrdatas = [google_compute_global_address.genegraph_gene_validity_stage_dev_external_ip.address]
-}
+#   rrdatas = [google_compute_global_address.genegraph_gene_validity_stage_dev_external_ip.address]
+# }
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -11,11 +11,11 @@ module "cloudbuild-firebase" {
   project_id        = data.google_project.current.project_id
 }
 
-module "external-secrets" {
-  source     = "../modules/external-secrets"
-  env        = "stage"
-  project_id = data.google_project.current.project_id
-}
+# module "external-secrets" {
+#   source     = "../modules/external-secrets"
+#   env        = "stage"
+#   project_id = data.google_project.current.project_id
+# }
 
 data "google_compute_default_service_account" "default_compute" {}
 
@@ -32,25 +32,31 @@ resource "google_artifact_registry_repository" "genegraph-stage" {
   format        = "DOCKER"
 }
 
-module "stage-gke-cluster" {
-  source                   = "github.com/broadinstitute/tgg-terraform-modules//imported-gke-cluster?ref=1679ea8bb0fedfb879bca581624c6c51df6efbfa"
-  cluster_name             = "stage-cluster"
-  cluster_location         = "us-east1-b"
-  network_id               = "projects/clingen-stage/global/networks/default"
-  subnetwork_id            = "projects/clingen-stage/regions/us-east1/subnetworks/default"
-  maint_start_time         = "2021-03-24T11:00:00Z"
-  maint_end_time           = "2021-03-24T23:00:00Z"
-  maint_recurrence_sched   = "FREQ=WEEKLY;BYDAY=SA,SU"
-  initial_node_count       = 0
-  remove_default_node_pool = true
-  cluster_v4_cidr          = "10.56.0.0/14"
-  services_v4_cidr         = "10.0.16.0/20"
-  resource_labels = {
-    admin      = "tristan"
-    creator    = "tristan"
-    managed_by = "terraform"
-  }
+resource "google_project_iam_member" "default_compute_sm_read" {
+  project = data.google_project.current.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = data.google_compute_default_service_account.default_compute.member
 }
+
+# module "stage-gke-cluster" {
+#   source                   = "github.com/broadinstitute/tgg-terraform-modules//imported-gke-cluster?ref=1679ea8bb0fedfb879bca581624c6c51df6efbfa"
+#   cluster_name             = "stage-cluster"
+#   cluster_location         = "us-east1-b"
+#   network_id               = "projects/clingen-stage/global/networks/default"
+#   subnetwork_id            = "projects/clingen-stage/regions/us-east1/subnetworks/default"
+#   maint_start_time         = "2021-03-24T11:00:00Z"
+#   maint_end_time           = "2021-03-24T23:00:00Z"
+#   maint_recurrence_sched   = "FREQ=WEEKLY;BYDAY=SA,SU"
+#   initial_node_count       = 0
+#   remove_default_node_pool = true
+#   cluster_v4_cidr          = "10.56.0.0/14"
+#   services_v4_cidr         = "10.0.16.0/20"
+#   resource_labels = {
+#     admin      = "tristan"
+#     creator    = "tristan"
+#     managed_by = "terraform"
+#   }
+# }
 
 # resource "google_container_node_pool" "main-node-pool" {
 #   name       = "main-node-pool"
@@ -62,35 +68,31 @@ module "stage-gke-cluster" {
 #     preemptible     = false
 #     machine_type    = "n2-standard-4"
 #     image_type      = "COS_CONTAINERD"
-#     ephemeral_storage_local_ssd_config {    
+#     ephemeral_storage_local_ssd_config {
 #       local_ssd_count = 1
 #     }
 #     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
 #   }
 # }
 
-resource "google_container_node_pool" "himem-node-pool" {
-  name       = "himem-node-pool"
-  location   = "us-east1-b"
-  cluster    = module.stage-gke-cluster.gke-cluster-name
-  node_count = 3
+# resource "google_container_node_pool" "himem-node-pool" {
+#   name       = "himem-node-pool"
+#   location   = "us-east1-b"
+#   cluster    = module.stage-gke-cluster.gke-cluster-name
+#   node_count = 3
 
-  node_config {
-    preemptible     = false
-    machine_type    = "n2-highmem-4"
-    image_type      = "COS_CONTAINERD"
-    ephemeral_storage_local_ssd_config {    
-      local_ssd_count = 1
-    }
-    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-}
+#   node_config {
+#     preemptible     = false
+#     machine_type    = "n2-highmem-4"
+#     image_type      = "COS_CONTAINERD"
+#     ephemeral_storage_local_ssd_config {
+#       local_ssd_count = 1
+#     }
+#     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+#   }
+# }
 
-resource "google_project_iam_member" "default_compute_sm_read" {
-  project = data.google_project.current.project_id
-  role    = "roles/secretmanager.secretAccessor"
-  member  = data.google_compute_default_service_account.default_compute.member
-}
+
 
 
 ##### Temporary cluster set up due to issue with main stage cluster
@@ -116,20 +118,20 @@ resource "google_project_iam_member" "default_compute_sm_read" {
 #   }
 # }
 
-resource "google_container_node_pool" "genegraph-stage-node-pool" {
-  name       = "arm-ssd-node-pool"
-  location   = "us-east1-b"
-  cluster    = module.stage-gke-cluster.gke-cluster-name
-  node_count = 1
-  node_config {
-    preemptible     = false
-    machine_type    = "c4a-highmem-4-lssd"
-    image_type      = "COS_CONTAINERD"
-    ### https://cloud.google.com/compute/docs/general-purpose-machines#c4a_machine_types
-    ### Should automatically configure one local ssd
-    # ephemeral_storage_local_ssd_config {    
-    #   local_ssd_count = 1
-    # }
-    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-}
+# resource "google_container_node_pool" "genegraph-stage-node-pool" {
+#   name       = "arm-ssd-node-pool"
+#   location   = "us-east1-b"
+#   cluster    = module.stage-gke-cluster.gke-cluster-name
+#   node_count = 1
+#   node_config {
+#     preemptible     = false
+#     machine_type    = "c4a-highmem-4-lssd"
+#     image_type      = "COS_CONTAINERD"
+#     ### https://cloud.google.com/compute/docs/general-purpose-machines#c4a_machine_types
+#     ### Should automatically configure one local ssd
+#     # ephemeral_storage_local_ssd_config {
+#     #   local_ssd_count = 1
+#     # }
+#     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+#   }
+# }
